dd3c66f0a9e4795dc34b28f6caa0a7fc67fc69e7
Xandra <xandra.macatangay@gmail.com>
2023-12-20 14:05:07 +1100

    used removeEventListener

--- modals.js
+++ modals.js
@@ -1,16 +1,17 @@
-const dialog = document.querySelector("dialog");
-const closeButton = document.querySelector(".close-btn");
-const closeApp = () => {
-  dialog.close();
-};
-closeButton.addEventListener("click", closeApp);
+// const dialog = document.querySelector("dialog");
+// const closeButton = document.querySelector(".close-btn");
+// const closeApp = () => {
+//   dialog.close();
+// };
+// closeButton.addEventListener("click", closeApp);
+//THE ABOVE FUNCTION WORKS TO CLOSE DIALOGS NORMALLY AKA 3RD GAME APP
 
 ///closing created modal
-const closeCreatedModal = () => {
-  document.getElementById("trying-out").removeChild(document.getElementById("test-child"));
-}
+// const closeCreatedModal = () => {
+//   document.getElementById("trying-out").removeChild(document.getElementById("test-child"));
+// }
 
-document.querySelector("#testing-btn").addEventListener("click", closeCreatedModal);
+// document.querySelector("#testing-btn").addEventListener("click", closeCreatedModal);
 /// cannot open game app with this, but opening top test app works; only cannot close
 
 
--- trying-out.js
+++ trying-out.js
@@ -6,12 +6,17 @@ function makeModal() {
   let testModal = document.createElement('dialog');
   // testModal.setAttribute("open", "");
   testModal.id = "test-child"
-  testModal.innerHTML = '<button class="close-btn" id="testing-btn">X</button><p>you can see me</p>';
+  testModal.innerHTML = '<button class="close-btn" id="testing-btn">X</button><p>you can see me</p><button id="newb">M</button>';
   document.querySelector("#trying-out").appendChild(testModal);
   testModal.showModal();
+  tryButton.removeEventListener("dblclick", makeModal)
 }
 
-tryButton.addEventListener("dblclick", () => {
-  makeModal();
-  //testModal.showModal();
-});
+tryButton.addEventListener("dblclick", makeModal);
+
+///////////closing dialog // seems to be no interaction with rendered elements
+const exitButton = document.querySelector("#newb");
+
+exitButton.addEventListener("click", () => {
+  alert ("click")
+})
