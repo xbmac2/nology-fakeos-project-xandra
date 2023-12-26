7d930963f282a9f6aaadad0924e774535b76468f
Xandra <xandra.macatangay@gmail.com>
2023-12-27 10:31:09 +1100

    fixed menu functionality

--- menu.js
+++ menu.js
@@ -1,18 +1,18 @@
 document.querySelector("#menu").style.display = "none";
 
-//shows menu
+//toggles menu with icon click
 document.querySelector("#menuButton").addEventListener("click", () => {
-  document.querySelector("#menu").style.display = "inline";
+  if (document.querySelector("#menu").style.display === "none") {
+    document.querySelector("#menu").style.display = "inline";
+  } else if (document.querySelector("#menu").style.display === "inline") {
+    document.querySelector("#menu").style.display = "none";
+  }
+  
 })
 
-// defining menu
-
-const menu = document.querySelector("#menu");
-
-// closes menu
-document.querySelector("#menuContainer").addEventListener("click", () => {
-  menu.style.display = "none";
+//closes menu
+document.addEventListener("click", e => {
+  if (!document.querySelector("#menu").contains(e.target) && e.target !== document.querySelector("#menuButton") && e.target !== document.querySelector("#menuImage")) {
+    document.querySelector("#menu").style.display = "none";
+  }
 })
-
-
-//if (menu.style.display === "inline") {};
